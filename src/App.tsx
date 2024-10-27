a1d12f99f4478658d57a865bb88ba77f45c75224
roodni <41773436+roodni@users.noreply.github.com>
2024-10-27 18:37:15 +0900

    suidやめる

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { createSignal } from 'solid-js';
-import { Button } from '@suid/material';
-import svg from './assets/solid.svg';
 
-function App() {
-  const [count, setCount] = createSignal(0);
+export default function SelectAutoWidth() {
+  const [age, setAge] = createSignal(1);
+
+  setAge(1);
 
   return (
-    <>
-      <h1>I DO NOT LIKE WEB FRONTENND ({count()})</h1>
-      <Button onClick={() => setCount(count() + 1)} variant='contained'>ボタン</Button>
-      <img src={svg}/>
-    </>
+    <div>
+      助けてくれ{age()}
+    </div>
   );
 }
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
-import suid from '@suid/vite-plugin';
 
 export default defineConfig({
-  plugins: [suid(), solid()],
+  plugins: [solid()],
 });
