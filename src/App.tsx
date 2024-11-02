3e9050fa380852aaf0b27ec2defafbc5b4126813
roodni <41773436+roodni@users.noreply.github.com>
2024-11-02 18:30:01 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,108 @@
-import { createSignal } from 'solid-js';
+// import { createSignal } from 'solid-js';
+import { Component } from 'solid-js';
+import './App.css';
 
-export default function SelectAutoWidth() {
-  const [age, setAge] = createSignal(1);
+const Textarea: Component<{}> = () => {
+  return (
+    <textarea
+      rows={7}
+      style={{
+        width: '100%',
+        resize: 'none',
+        display: 'block'
+      }}
+    >
+    </textarea>
+  );
+};
 
-  setAge(1);
+export default function App() {
+  const headerHeight = '3rem';
+  const leftFooterHeight = '3rem';
 
   return (
-    <div>
-      助けてくれ{age()}
+    <div style={{
+      display: 'flex',
+      'flex-wrap': 'wrap'
+    }}>
+      {/* ヘッダー */}
+      <div
+          style={{
+            width: '100%',
+            height: headerHeight,
+            'background-color': 'burlywood'
+          }}
+          class='valign-center'
+        >
+        <div style={{'font-size': '1.5rem'}} class='right-box'>
+          Reusable-bf Playground
+        </div>
+      </div>
+      
+      {/* 左 */}
+      <div
+        style={{
+          width: '50%',
+          height: `calc(100svh - ${headerHeight})`,
+          'background-color': 'olive',
+          padding: '4px',
+        }}
+      >
+        <div
+          style={{
+            height: `calc(100% - ${leftFooterHeight})`,
+          }}
+        >
+          <textarea
+            style={{
+              width: '100%',
+              height: '100%',
+              resize: 'none',
+            }}
+          >
+          </textarea>
+        </div>
+        <div
+          style={{height: leftFooterHeight}}
+          class='valign-center'
+        >
+          <select class='input'>
+            <option>sandbox.bfml</option>
+            <option>examples/hello.bfml</option>
+            <option>std.bfml</option>
+          </select>
+          <button class='input'>
+            Compile
+          </button>
+        </div>
+      </div>
+
+      {/* 右 */}
+      <div
+        style={{
+          width: '50%',
+          height: `calc(100svh - ${headerHeight})`,
+          'background-color': 'peachpuff',
+        }}
+      >
+        <div class='right-box'>
+          Error
+        </div>
+        <div class='right-box'>
+          brainf**k:
+          <Textarea></Textarea>
+        </div>
+        <div class='right-box'>
+          Input:
+          <Textarea></Textarea>
+          <button class='input'>Run</button>
+          <button class='input' disabled>Stop</button>
+        </div>
+        <div class='right-box'>
+          Output:
+          <Textarea></Textarea>
+        </div>
+      </div>
     </div>
   );
 }
